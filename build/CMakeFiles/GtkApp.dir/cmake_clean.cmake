d60fb5d9da8c246ada3f62eeadb9b21223b44a3c
Fajar Julyana <fajarjulyana@github.com>
2023-12-25 19:45:31 +0700

    build CMakeLists.txt LICENSE README.md src ui added

---
+++ build/CMakeFiles/GtkApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GtkApp.dir/src/main.c.o"
+  "CMakeFiles/GtkApp.dir/src/main.c.o.d"
+  "GtkApp"
+  "GtkApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GtkApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/GtkApp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GtkApp.
+# This may be replaced when dependencies are built.
